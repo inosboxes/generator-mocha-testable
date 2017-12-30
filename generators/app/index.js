cce1dd5d0c813a732cbba66d390beede008d6645
Xmonad Jump Virtual Box <xmonad-jumpbox@trackingdesk.com>
2017-12-30 19:43:54 +0000

    fixing eslint format

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,16 +6,16 @@ const yosay = require('yosay');
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the astounding ' + chalk.red('generator-mocha-testable-class') + ' generator!'
-    ));
+    yosay('Welcome to ' + chalk.red('generator-mocha-testable-class') + ' generator!');
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    const prompts = [
+      {
+        type: 'confirm',
+        name: 'someAnswer',
+        message: 'Would you like to enable this option?',
+        default: true
+      }
+    ];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
